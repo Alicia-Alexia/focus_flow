9fb4e2b0ac20ad04023680086573a156b9aa99cd
alicia <alicia.alexia03@gmail.com>
2026-05-14 17:05:27 -0300

    feat(api): register GET /tasks/:id route

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -9,6 +9,7 @@ export async function taskRoutes(app: FastifyInstance) {
   app.post('/users', authController.register);
   app.post('/sessions', authController.login);
   app.get('/tasks', taskController.list);
+  app.get('/tasks/:id', taskController.getById)
   app.post('/tasks', taskController.create);
   app.patch('/tasks/:id', taskController.update);
   app.delete('/tasks/:id', taskController.delete);
